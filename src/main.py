01461ebf1d7cb58f752e51143165ae36f256daa1
Evgeniy Skvortsov <jonnyzver78@gmail.com>
2024-01-13 11:35:18 +1000

    initial

---
+++ src/main.py
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+Hello Linux
+=======
+>>>>>>> parent of 54d807c (initial)
+=======
+>>>>>>> parent of 54d807c (initial)
+=======
+>>>>>>> parent of 54d807c (initial)
+=======
+>>>>>>> parent of c9ba5e5 (hello word)
+=======
+>>>>>>> parent of c9ba5e5 (hello word)
